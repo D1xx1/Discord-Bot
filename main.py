4cbde766fce4d3646a88f99a110ebe1ad255f145
Viktor <admin@d1x1.ru>
2022-03-24 01:28:48 +0700

    24.03.2022
    Clearing

--- main.py
+++ main.py
@@ -3,40 +3,32 @@ import discord
 from discord.ext import commands
 from config import settings
 import random
-import roles
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
-    
-    async def on_raw_reaction_add(self, payload):
-        pass
-
-    async def on_raw_reaction_remove(self, payload):
-        pass
-
 
 fkbot = commands.Bot(command_prefix = settings['prefix'])
+
+
 @fkbot.command()
 async def hello(ctx):
     author = ctx.message.author
     await ctx.send(f'Hello, {author.mention}!')
+
 @fkbot.command()
 async def привет(ctx):
     author = ctx.message.author
     await ctx.send(f'Привет, {author.mention}')
+
 @fkbot.command()
 async def roll(ctx, arg='100'):
-        try:
-            if int(arg) < 0:
-                await ctx.send(random.randint(int(arg), 0))
-                return
-            else: 
-                await ctx.send(random.randint(0,int(arg)))
-                return
-        except ValueError:
-            author = ctx.message.author
-            await ctx.send(f'{author.mention}, введи число!')
+    try:
+        if int(arg) < 0:
+            await ctx.send(random.randint(int(arg), 0))
             return
-#@fkbot.command()
-Client = MyClient()
+        else: 
+            await ctx.send(random.randint(0,int(arg)))
+            return
+    except ValueError:
+        author = ctx.message.author
+        await ctx.send(f'{author.mention}, введи число!')
+        return
+
 fkbot.run(settings['token'])
